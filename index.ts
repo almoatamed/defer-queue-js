502d4630ea044e30977dfbcaec8adbdb276bc506
almoatamed <73703711+almoatamed@users.noreply.github.com>
2025-10-20 00:59:20 +0200

    made sure to execute sync function in reverse order last in first out (LIFO/FILO), also added results that is returned

--- index.ts
+++ index.ts
@@ -15,25 +15,49 @@ export class DeferQueue {
     }
 
     async defer() {
+        const results:( {
+            type: "success"; 
+            result: any; 
+        } | {
+            type: "failed"; 
+            error: any; 
+        })[] = []
         await Promise.all([
             ...this.asyncList.map(async (cb) => {
                 try {
-                    await cb();
+                   const  result =  await cb();
+                   results.push({
+                    type: "success", 
+                    result, 
+                   })
                 } catch (error) {
                     console.error(this.name, "failed to perform async deferred function", error);
+                    results.push({
+                        type: "failed", 
+                        error,
+                    })
                 }
             }),
             (async () => {
                 while (this.syncList.length) {
-                    const cb = this.syncList.shift()!;
+                    const cb = this.syncList.pop()!;
                     try {
-                        await cb();
+                        const result = await cb();
+                        results.push({
+                            type: "success", 
+                            result,
+                        })
                     } catch (error) {
                         console.error(this.name, "Failed to run sync deferred callback", error);
+                        results.push({
+                            type: 'failed', 
+                            error,
+                        })
                     }
                 }
             })(),
         ]);
+        return results; 
     }
 }
 
